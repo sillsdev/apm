07f66fdd64e67f52ca2e0c21cc4b494e0c291b52
Greg Trihus <gtryus@gmail.com>
2025-09-26 16:56:24 -0500

    update write date and clean

--- env-config/writeDate.js
+++ env-config/writeDate.js
@@ -3,4 +3,4 @@ var { DateTime } = require('luxon');
 var dt = DateTime.now().toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
 var tm = DateTime.now().toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET);
 var content = `{ "date": "${dt} ${tm}" }`;
-writeFile.sync('src/buildDate.json', content);
+writeFile.sync('src/renderer/src/buildDate.json', content);
