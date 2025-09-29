7704aead89e0887335fff3b892fc63a056465974
Greg Trihus <gtryus@gmail.com>
2025-09-29 10:35:14 -0500

    update channel change scripts

--- env-config/changeEnv.cjs
+++ env-config/changeEnv.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const fs = require('fs');
 
 var argEnv = process.argv.length > 2 ? process.argv[2] : 'dev';
@@ -19,78 +20,37 @@ fs.copyFile(
     );
   }
 );
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile(
+  `env-config/.env.${argEnv}.local`,
+  'src/renderer/.env.local',
+  (err) => {
+    if (err) throw err;
+    console.log(`env-config/.env.${argEnv}.local was copied to .env.local`);
+  }
+);
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile(
+  `env-config/.env.${argEnv}.development.local`,
+  'src/renderer/.env.development.local',
+  (err) => {
+    if (err) throw err;
+    console.log(
+      `env-config/.env.${argEnv}.development.local was copied to .env.development.local`
+    );
+  }
+);
+
+// destination.txt will be created or overwritten by default.
 fs.copyFile(
   `env-config/.auth0-variables.${argEnv}.json`,
-  `src/auth/auth0-variables.json`,
+  `src/main/auth0-variables.json`,
   (err) => {
     if (err) throw err;
     console.log(
       `env-config/.auth0-variables.${argEnv}.json was copied to src/auth/auth0-variables.json`
-    );
-  }
-);
-fs.copyFile(
-  `env-config/.auth0-variables.${argEnv}.json`,
-  `public/auth0-variables.json`,
-  (err) => {
-    if (err) throw err;
-    console.log(
-      `env-config/.auth0-variables.${argEnv}.json was copied to public/auth0-variables.json`
-    );
-  }
-);
-// destination.txt will be created or overwritten by default.
-fs.copyFile(
-  'amplify/' + argEnv + '/amplify-meta.json',
-  'amplify/backend/amplify-meta.json',
-  (err) => {
-    if (err) throw err;
-    console.log(
-      'amplify/' +
-        argEnv +
-        '/amplify-meta.json was copied to amplify/backend/amplify-meta.json'
-    );
-  }
-);
-
-// destination.txt will be created or overwritten by default.
-fs.copyFile(
-  'amplify/' + argEnv + '/parameters.json',
-  'amplify/backend/hosting/S3AndCloudFront/parameters.json',
-  (err) => {
-    if (err) throw err;
-    console.log(
-      'amplify/' +
-        argEnv +
-        '/parameters.json was copied to amplify/backend/hosting/S3AndCloudFront/parameters.json'
-    );
-  }
-);
-
-// destination.txt will be created or overwritten by default.
-fs.copyFile(
-  'amplify/' + argEnv + '/amplify-meta.json',
-  'amplify/#current-cloud-backend/amplify-meta.json',
-  (err) => {
-    if (err) throw err;
-    console.log(
-      'amplify/' +
-        argEnv +
-        '/amplify-meta.json was copied to amplify/#current-cloud-backend/amplify-meta.json'
-    );
-  }
-);
-
-// destination.txt will be created or overwritten by default.
-fs.copyFile(
-  'amplify/' + argEnv + '/parameters.json',
-  'amplify/#current-cloud-backend/hosting/S3AndCloudFront/parameters.json',
-  (err) => {
-    if (err) throw err;
-    console.log(
-      'amplify/' +
-        argEnv +
-        '/parameters.json was copied to amplify/#current-cloud-backend/hosting/S3AndCloudFront/parameters.json'
     );
   }
 );
