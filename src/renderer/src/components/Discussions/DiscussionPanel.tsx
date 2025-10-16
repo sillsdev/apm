9ed481ba687577325bb03e1c8a0bb84a92ff9b75
Greg Trihus <gtryus@gmail.com>
2025-10-16 12:34:15 -0500

    TT-6855 update tool tips - show when no discussions present

--- src/renderer/src/components/Discussions/DiscussionPanel.tsx
+++ src/renderer/src/components/Discussions/DiscussionPanel.tsx
@@ -66,7 +66,7 @@ export default function DiscussionPanel() {
             </Fab>
           </Badge>
         ) : (
-          <Fab size="small" onClick={() => setDiscussOpen(true)}>
+          <Fab size="small" onClick={() => setDiscussOpen(true)} title={t.open}>
             <DiscussIcon />
           </Fab>
         )}
