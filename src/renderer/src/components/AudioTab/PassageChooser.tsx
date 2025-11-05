a6af8ecfb6cd991b513106abcff524c1be398881
Greg Trihus <gtryus@gmail.com>
2025-11-05 17:07:25 -0600

    TT-6898 Passage Chooser Layout

--- src/renderer/src/components/AudioTab/PassageChooser.tsx
+++ src/renderer/src/components/AudioTab/PassageChooser.tsx
@@ -1,6 +1,6 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { IMediaTabStrings, IState, PassageD } from '../../model';
-import { FormControlLabel, Switch } from '@mui/material';
+import { Box, debounce, FormControlLabel, Switch } from '@mui/material';
 import { findRecord, useOrganizedBy } from '../../crud';
 import { GetReference, IPRow } from '.';
 import { useSelector } from 'react-redux';
@@ -12,6 +12,7 @@ import {
   type GridSortModel,
   type GridRowSelectionModel,
   GridRenderCellParams,
+  GridColumnVisibilityModel,
 } from '@mui/x-data-grid';
 import { useGlobal } from '../../context/useGlobal';
 
@@ -35,6 +36,13 @@ export const PassageChooser = (props: IProps) => {
   const { getOrganizedBy } = useOrganizedBy();
   const [organizedBy] = useState(getOrganizedBy(true));
   const [pcheck, setCheck] = useState(-1);
+  const boxRef = useRef<HTMLDivElement>(null);
+  const [addWidth, setAddWidth] = useState(0);
+
+  const [columnVisibilityModel] = useState<GridColumnVisibilityModel>({
+    sort: false,
+    attached: false,
+  });
   const [selectedRows, setSelectedRows] = useState<GridRowSelectionModel>({
     type: 'include',
     ids: new Set(),
@@ -51,19 +59,21 @@ export const PassageChooser = (props: IProps) => {
     );
   };
 
+  const MinNameWidth = 150;
+
   const columns: GridColDef<IPRow>[] = useMemo(
     () => [
       {
         field: 'sectionDesc',
         headerName: organizedBy,
-        width: 150,
+        width: MinNameWidth + addWidth / 2,
         align: 'left',
         cellClassName: 'word-wrap',
       },
       {
         field: 'reference',
         headerName: t.reference,
-        width: 150,
+        width: MinNameWidth + addWidth / 2,
         align: 'left',
         cellClassName: 'word-wrap',
         type: 'singleSelect',
@@ -78,7 +88,7 @@ export const PassageChooser = (props: IProps) => {
       { field: 'sort', headerName: '\u00A0', width: 100, align: 'left' },
     ],
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [organizedBy]
+    [organizedBy, addWidth]
   );
 
   const sortModel: GridSortModel = [{ field: 'sort', sort: 'asc' }];
@@ -89,6 +99,47 @@ export const PassageChooser = (props: IProps) => {
     operator: 'contains',
     value: 'N',
   });
+
+  const totalWidth = useMemo(
+    () =>
+      columns.reduce(
+        (sum, col) =>
+          !columnVisibilityModel[col.field]
+            ? 0
+            : ['sectionDesc', 'reference'].includes(col.field)
+              ? sum + MinNameWidth
+              : sum + (col.width ?? 0),
+        0
+      ),
+    [columns, columnVisibilityModel]
+  );
+
+  const ExtraWidth = 0;
+
+  // keep track of screen width
+  const setDimensions = () => {
+    const boxWidth = boxRef.current?.clientWidth ?? 0;
+    setAddWidth(boxWidth > totalWidth ? boxWidth - totalWidth - ExtraWidth : 0);
+  };
+
+  useEffect(() => {
+    setDimensions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [totalWidth]);
+
+  useEffect(() => {
+    setDimensions();
+    const handleResize = debounce(() => {
+      setDimensions();
+    }, 100);
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      handleResize.clear();
+      window.removeEventListener('resize', handleResize);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); //do this once to get the default;
 
   const handleAttachedFilterChange = (e: any) => {
     setAttachedFilter({
@@ -124,7 +175,7 @@ export const PassageChooser = (props: IProps) => {
   };
 
   return (
-    <div>
+    <Box ref={boxRef}>
       <FormControlLabel
         value="attached"
         labelPlacement="end"
@@ -142,7 +193,7 @@ export const PassageChooser = (props: IProps) => {
         filterModel={{ items: [attachedFilter] }}
         initialState={{
           sorting: { sortModel },
-          columns: { columnVisibilityModel: { sort: false, attached: false } },
+          columns: { columnVisibilityModel },
         }}
         checkboxSelection
         disableRowSelectionOnClick
@@ -150,7 +201,7 @@ export const PassageChooser = (props: IProps) => {
         rowSelectionModel={selectedRows}
         sx={{ '& .wrap-text': { whiteSpace: 'break-spaces' } }}
       />
-    </div>
+    </Box>
   );
 };
 
