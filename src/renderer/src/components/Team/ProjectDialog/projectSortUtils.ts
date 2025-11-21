933fc324609b2b4620c8d4cf3b8610844ed3b9fb
Sara Hentzel <sara_hentzel@sil.org>
2025-11-21 14:33:30 -0600

    TT-6938 fix project delete - database no longer cascades deletes on plan delete

--- src/renderer/src/components/Team/ProjectDialog/projectSortUtils.ts
+++ src/renderer/src/components/Team/ProjectDialog/projectSortUtils.ts
@@ -3,5 +3,6 @@ import { ProjectD } from '../../../model';
 export type SortArr = [string, number][];
 export type SortMap = Map<string, number>;
 
-export const mapKey = (p: ProjectD) => p.keys?.remoteId || p.id;
-export const getKey = (p: ProjectD, map: SortMap) => map.get(mapKey(p));
+export const mapKey = (p: ProjectD) => p?.keys?.remoteId || p?.id;
+export const getKey = (p: ProjectD, map: SortMap) =>
+  mapKey(p) ? map.get(mapKey(p)) : undefined;
