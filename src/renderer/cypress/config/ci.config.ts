9504bba4246d65f3cd35b19a003459db914f9190
gtryus <gtrihus59@yahoo.com>
2025-12-09 14:49:00 -0600

    convert dev build from Team City (#150)
    * convert dev build from Team City
    
    * install root dependencies too
    
    * build index.html with dev
    
    * run index.html script from root
    
    * correct script path for index.html
    
    * create special ci test build for cypress
    
    * add a delay to devserver launch
    
    * add a delay to devserver launch
    
    * add wait on for dev server port
    
    * update package lock file
    
    * update cypress to include vite dev server
    
    * try wait-on again
    
    * using docker image
    
    * skip component testing for now
    
    * fix validate issue
    
    * remove validate
    
    ---------
    
    Co-authored-by: Greg Trihus <gtryus@gmail.com>

---
+++ src/renderer/cypress/config/ci.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'cypress';
+import { devServer } from '@cypress/vite-dev-server';
+import { baseConfig } from './base.config';
+import viteConfig from '../../vite.config';
+import path from 'node:path';
+import merge from 'lodash/merge';
+
+// Merge the base vite config with test-specific overrides
+const testViteConfig = {
+  ...viteConfig,
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('test'),
+    'process.env.FA_VERSION': JSON.stringify('test-version'),
+  },
+  resolve: {
+    ...viteConfig.resolve,
+    alias: {
+      ...viteConfig.resolve?.alias,
+      // Break circular dependency: NavRoutes imports SwitchTeams, but we're testing SwitchTeams
+      // Mock NavRoutes to prevent the circular dependency during component tests
+      '../routes/NavRoutes': path.resolve(
+        __dirname,
+        '../../src/routes/__mocks__/NavRoutes.tsx'
+      ),
+      './routes/NavRoutes': path.resolve(
+        __dirname,
+        '../../src/routes/__mocks__/NavRoutes.tsx'
+      ),
+    },
+  },
+};
+
+const config = {
+  e2e: {
+    env: {
+      ENVIRONMENT: 'ci',
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+  component: {
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+    devServer(devServerConfig: Cypress.DevServerConfig) {
+      return devServer({
+        ...devServerConfig,
+        framework: 'react',
+        viteConfig: testViteConfig,
+      });
+    },
+  },
+};
+
+export default defineConfig(merge({}, baseConfig, config));
