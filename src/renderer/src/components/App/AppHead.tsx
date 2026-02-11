aba81605bea938a6cb30b7ce7a19cf92f17a6f82
Sara Hentzel <sara_hentzel@sil.org>
2026-02-11 09:44:07 -0600

    move toolbars to their own components

--- src/renderer/src/components/App/AppHead.tsx
+++ src/renderer/src/components/App/AppHead.tsx
@@ -1,26 +1,16 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { useGetGlobal, useGlobal } from '../../context/useGlobal';
-import { useLocation, useParams } from 'react-router-dom';
-import { IState, IViewModeStrings, RoleNames } from '../../model';
+import { useLocation } from 'react-router-dom';
+import { IState, IViewModeStrings } from '../../model';
 import { shallowEqual, useSelector } from 'react-redux';
 import {
   AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
   LinearProgress,
-  Tooltip,
   Box,
 } from '@mui/material';
-import HomeIcon from '@mui/icons-material/Home';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import TableViewIcon from '@mui/icons-material/TableView';
-import { API_CONFIG, isElectron } from '../../../api-variable';
+import { isElectron } from '../../../api-variable';
 import { TokenContext } from '../../context/TokenProvider';
 import { UnsavedContext } from '../../context/UnsavedContext';
-import HelpMenu from '../HelpMenu';
-import UserMenu from '../UserMenu';
-import { GrowingSpacer } from '../../control';
 import {
   resetData,
   exitElectronApp,
@@ -36,200 +26,17 @@ import {
   useMobile,
 } from '../../utils';
 import { withBucket } from '../../hoc/withBucket';
-import { usePlan } from '../../crud';
 import Busy from '../Busy';
 import ProjectDownloadAlert from '../ProjectDownloadAlert';
 import { useSnackBar } from '../../hoc/SnackBar';
 import PolicyDialog from '../PolicyDialog';
 import JSONAPISource from '@orbit/jsonapi';
 import { viewModeSelector } from '../../selector';
-import { useHome } from '../../utils/useHome';
-import { ApmLogo } from '../../control/ApmLogo';
-import { OrgHead } from './OrgHead';
-import { HeadStatus } from './HeadStatus';
+import { DesktopToolbar } from './DesktopToolbar';
+import { MobileToolbar } from './MobileToolbar';
 
 const twoIcon = { minWidth: `calc(${48 * 2}px)` } as React.CSSProperties;
 const threeIcon = { minWidth: `calc(${48 * 3}px)` } as React.CSSProperties;
-
-interface INameProps {
-  switchTo: boolean;
-}
-
-const ProjectName = ({ switchTo }: INameProps) => {
-  const ctx = useContext(UnsavedContext);
-  const { checkSavedFn } = ctx.state;
-  const { getPlanName } = usePlan();
-  const [plan] = useGlobal('plan'); //verified this is not used in a function 2/18/25
-  const { prjId } = useParams();
-  const navigate = useMyNavigate();
-  const { goHome } = useHome();
-  const t: IViewModeStrings = useSelector(viewModeSelector, shallowEqual);
-
-  const handleHome = () => {
-    localStorage.removeItem(LocalKey.plan);
-    localStorage.removeItem('mode');
-    goHome();
-  };
-
-  const handleAudioProject = () => {
-    navigate(`/plan/${prjId}/0`);
-  };
-
-  const checkSavedAndGoAP = () => checkSavedFn(() => handleAudioProject());
-  const checkSavedAndGoHome = () => checkSavedFn(() => handleHome());
-
-  return (
-    <>
-      <Tooltip title={t.home}>
-        <IconButton id="home" onClick={checkSavedAndGoHome}>
-          <HomeIcon />
-        </IconButton>
-      </Tooltip>
-      {plan && switchTo && (
-        <Tooltip title={t.audioProject}>
-          <IconButton id="project" onClick={checkSavedAndGoAP}>
-            <TableViewIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-      <Typography variant="h6" noWrap>
-        {getPlanName(plan)}
-      </Typography>
-    </>
-  );
-};
-
-interface DesktopToolbarProps {
-  switchTo?: boolean;
-  home: boolean;
-  orgRole: RoleNames | undefined;
-  cssVars: React.CSSProperties;
-  pathname: string;
-  handleMenu: (what: string, reason?: DownloadAlertReason | null) => void;
-  setVersion: (version: string) => void;
-  setLatestVersion: (version: string) => void;
-  setUpdateTipOpen: (open: boolean) => void;
-  isOffline: boolean;
-  updateTipOpen: boolean;
-  handleUserMenu: (what: string) => void;
-  tv: IViewModeStrings;
-}
-
-const DesktopToolbar = ({
-  switchTo,
-  home,
-  orgRole,
-  cssVars,
-  pathname,
-  handleMenu,
-  setVersion,
-  setLatestVersion,
-  setUpdateTipOpen,
-  isOffline,
-  updateTipOpen,
-  handleUserMenu,
-  tv,
-}: DesktopToolbarProps) => {
-  return (
-    <Toolbar>
-      {!home && orgRole && (
-        <>
-          <ProjectName switchTo={switchTo ?? false} />
-          <GrowingSpacer />
-          <Typography variant="h6">
-            {switchTo ? tv.work : tv.audioProject}
-          </Typography>
-          <GrowingSpacer />
-        </>
-      )}
-      {home && <span style={cssVars}>{'\u00A0'}</span>}
-      <GrowingSpacer />
-      {(pathname === '/' || pathname.startsWith('/access')) && (
-        <>
-          <Typography variant="h6" noWrap>
-            {API_CONFIG.productName}
-          </Typography>
-          <GrowingSpacer />
-        </>
-      )}
-      {'\u00A0'}
-      <HeadStatus
-        handleMenu={handleMenu}
-        onVersion={setVersion}
-        onLatestVersion={setLatestVersion}
-        onUpdateTipOpen={setUpdateTipOpen}
-      />
-      <HelpMenu
-        online={!isOffline}
-        sx={updateTipOpen && isElectron ? { top: '40px' } : {}}
-      />
-      {pathname !== '/' && !pathname.startsWith('/access') && (
-        <UserMenu action={handleUserMenu} />
-      )}
-    </Toolbar>
-  );
-};
-
-interface MobileToolbarProps {
-  isDetail: boolean;
-  planUrl: string | null;
-  navigate: (path: string) => void;
-  isMobileWidth: boolean;
-  handleMenu: (what: string, reason?: DownloadAlertReason | null) => void;
-  setVersion: (version: string) => void;
-  setLatestVersion: (version: string) => void;
-  setUpdateTipOpen: (open: boolean) => void;
-  isOffline: boolean;
-  updateTipOpen: boolean;
-  pathname: string;
-  handleUserMenu: (what: string) => void;
-}
-
-const MobileToolbar = ({
-  isDetail,
-  planUrl,
-  navigate,
-  isMobileWidth,
-  handleMenu,
-  setVersion,
-  setLatestVersion,
-  setUpdateTipOpen,
-  isOffline,
-  updateTipOpen,
-  pathname,
-  handleUserMenu,
-}: MobileToolbarProps) => {
-  return (
-    <Toolbar>
-      {!isDetail ? (
-        <IconButton onClick={() => navigate('/team')} sx={{ p: 0 }}>
-          <ApmLogo sx={{ width: '24px', height: '24px' }} />
-        </IconButton>
-      ) : (
-        <IconButton onClick={() => navigate(planUrl || '/team')}>
-          <ArrowBackIcon sx={{ width: '24px', height: '24px' }} />
-        </IconButton>
-      )}
-      <OrgHead />
-      <GrowingSpacer />
-      {!isMobileWidth && (
-        <HeadStatus
-          handleMenu={handleMenu}
-          onVersion={setVersion}
-          onLatestVersion={setLatestVersion}
-          onUpdateTipOpen={setUpdateTipOpen}
-        />
-      )}
-      <HelpMenu
-        online={!isOffline}
-        sx={updateTipOpen && isElectron ? { top: '40px' } : {}}
-      />
-      {pathname !== '/' && !pathname.startsWith('/access') && (
-        <UserMenu action={handleUserMenu} small={true} />
-      )}
-    </Toolbar>
-  );
-};
 
 type ResetRequests = () => Promise<void>;
 
