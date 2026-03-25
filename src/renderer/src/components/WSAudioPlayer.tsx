fea7316e05efea7225df6d5c21a6ee404edf20f8
Sara Hentzel <sara_hentzel@sil.org>
2026-03-25 13:36:44 -0500

    TT-6981 wait for undo to complete

--- src/renderer/src/components/WSAudioPlayer.tsx
+++ src/renderer/src/components/WSAudioPlayer.tsx
@@ -1097,7 +1097,6 @@ function WSAudioPlayer(props: IProps) {
   };
 
   const handleChanged = useCallback(async () => {
-    setChanged && setChanged(durationRef.current !== 0);
     setBlobReady && setBlobReady(false);
     wsBlob().then((newblob) => {
       onBlobReady && onBlobReady(newblob);
@@ -1105,6 +1104,7 @@ function WSAudioPlayer(props: IProps) {
       if (setMimeType && newblob?.type) setMimeType(newblob?.type);
       setDuration(wsDuration());
       setProgress(wsPosition());
+      setChanged && setChanged(durationRef.current !== 0);
     });
   }, [
     setChanged,
@@ -1163,8 +1163,9 @@ function WSAudioPlayer(props: IProps) {
     });
   };
   const handleUndo = useCallback(() => {
-    wsUndo();
-    handleChanged();
+    wsUndo().then(() => {
+      handleChanged();
+    });
   }, [wsUndo, handleChanged]);
   useEffect(() => {
     if (!controlsRef) return;
