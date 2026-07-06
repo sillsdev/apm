ecff7b3628715fc414153e736100fa6bf9d7b44b
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-07-06 16:06:26 +0900

    TT-7476 Set a default avatar size

--- src/renderer/src/control/avatarSize.ts
+++ src/renderer/src/control/avatarSize.ts
@@ -1,7 +1,5 @@
 import { SxProps } from '@mui/material';
 
-const smallProps = { width: '24px', height: '24px' } as SxProps;
-const mediumProps = { width: '48px', height: '48px' } as SxProps;
+const avatarProps = { width: '24px', height: '24px' } as SxProps;
 
-export const avatarSize = (small?: boolean) =>
-  small ? smallProps : mediumProps;
+export const avatarSize = () => avatarProps;
