a4cb63605c2836686fa4222a195d9288dfa755c2
Greg Trihus <gtryus@gmail.com>
2026-07-11 15:39:40 -0500

    TT-7015 feat: enhance HTMLPage and PolicyDialog components with responsive design and mobile support

--- src/renderer/src/components/PolicyDialog.tsx
+++ src/renderer/src/components/PolicyDialog.tsx
@@ -1,7 +1,9 @@
 import { BigDialog } from '../hoc/BigDialog';
+import { BigDialogBp } from '../hoc/BigDialogBp';
 import { HTMLPage } from './HTMLPage';
 import { termsContent } from '../routes/TermsContent';
 import { privacyContent } from '../routes/privacyContent';
+import { useMobile } from '../utils';
 
 interface PolicyDialogProps {
   isOpen: boolean;
@@ -11,9 +13,16 @@ interface PolicyDialogProps {
 
 export const PolicyDialog = (props: PolicyDialogProps) => {
   const { isOpen, content, onClose } = props;
+  const { isMobile } = useMobile();
 
   return (
-    <BigDialog title="" isOpen={isOpen} onOpen={onClose}>
+    <BigDialog
+      title=""
+      isOpen={isOpen}
+      onOpen={onClose}
+      bp={isMobile ? BigDialogBp.mobile : BigDialogBp.sm}
+      mobileNoHorizontalScroll
+    >
       <HTMLPage text={/terms/i.test(content) ? termsContent : privacyContent} />
     </BigDialog>
   );
