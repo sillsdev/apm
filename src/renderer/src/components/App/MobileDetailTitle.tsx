8616be8c408031a05fc2265c9ab0c1d4a46dc976
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-08-04 14:59:02 -0500

    TT-7493 Get rid of manual Mobile Detail Title sizing

--- src/renderer/src/components/App/MobileDetailTitle.tsx
+++ src/renderer/src/components/App/MobileDetailTitle.tsx
@@ -22,16 +22,11 @@ export default function MobileDetailTitle() {
     projects.find((p) => p.id === project)?.attributes?.name ?? '';
 
   return (
-    <Box sx={{ ml: 1, minWidth: 0, overflow: 'hidden' }}>
-      <Typography variant="subtitle2" noWrap sx={{ lineHeight: 1.1 }}>
+    <Box sx={{ minWidth: 0, overflow: 'hidden' }}>
+      <Typography variant="subtitle2" noWrap>
         {reference}
       </Typography>
-      <Typography
-        variant="caption"
-        color="text.secondary"
-        noWrap
-        sx={{ lineHeight: 1.1 }}
-      >
+      <Typography variant="caption" color="text.secondary" noWrap>
         {projectName}
       </Typography>
     </Box>
