fbb2a95e0e780192767f0bef159d290ed989b8a1
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-08-11 14:40:23 -0500

    Use better variable name

--- src/renderer/src/control/layoutSx.ts
+++ src/renderer/src/control/layoutSx.ts
@@ -13,7 +13,7 @@ export const spreadSx = (theme: Theme) => ({
 });
 
 // For flexible content (e.g., contains text that can truncate)
-export const growSx = { flex: '1 1 auto', minWidth: 0 };
+export const flexibleSx = { flex: '1 1 auto', minWidth: 0 };
 
 // For rigid content (e.g., only contains icon buttons)
 export const rigidSx = { flexShrink: 0 };
