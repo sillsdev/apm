68cf1ef66f222c9938da3716d0ad1af6801b985a
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-08-28 11:13:58 -0500

    Don't do star import

--- src/renderer/src/burrito/MetaValue.tsx
+++ src/renderer/src/burrito/MetaValue.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -25,8 +25,8 @@ export default function MetaValue({
   isOpen,
   onOpen,
 }: MetaValueProps) {
-  const [open, setOpen] = React.useState(isOpen);
-  const [newValue, setNewValue] = React.useState(value);
+  const [open, setOpen] = useState(isOpen);
+  const [newValue, setNewValue] = useState(value);
   const t: IBurritoStrings = useSelector(burritoSelector, shallowEqual);
   const ts: ISharedStrings = useSelector(sharedSelector, shallowEqual);
 
@@ -35,11 +35,11 @@ export default function MetaValue({
     onOpen(false);
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     setOpen(isOpen);
   }, [isOpen]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setNewValue(value);
   }, [value]);
 
