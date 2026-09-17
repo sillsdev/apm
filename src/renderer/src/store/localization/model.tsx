a2f539294a6bc8e4540c86e07d75d9e4f1163a78
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-09-17 14:12:09 -0500

    TT-7547 Regenerate localization output for auto transcription strings

--- src/renderer/src/store/localization/model.tsx
+++ src/renderer/src/store/localization/model.tsx
@@ -2162,6 +2162,7 @@ export interface IWsAudioPlayerStrings extends Localize.LocalizedStringsMethods 
     "aiFailed": string;
     "aiGiveUp": string;
     "aiInProgress": string;
+    "autoTranscription": string;
     "beginVoiceConvert": string;
     "backTip": string;
     "convertVoice": string;
