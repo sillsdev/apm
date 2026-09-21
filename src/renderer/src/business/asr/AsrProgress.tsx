e1d60bc5d02b600f4ae4cf9d6e4d6040d4bef09b
Sara Hentzel <sara_hentzel@sil.org>
2026-09-21 09:17:01 -0500

    prettier

--- src/renderer/src/business/asr/AsrProgress.tsx
+++ src/renderer/src/business/asr/AsrProgress.tsx
@@ -144,8 +144,7 @@ export default function AsrProgress({
 
   const clearTrTasks = async () => {
     const mediaRec = findRecord(memory, 'mediafile', mediaId) as
-      | MediaFileD
-      | undefined;
+      MediaFileD | undefined;
     if (!mediaRec) throw new Error('Mediafile not found');
     const segments = updateSegments(
       NamedRegions.TRTask,
